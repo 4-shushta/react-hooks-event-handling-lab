dc669d661ad58e7dc2a935b6983f28777bf9879b
4-shushta <knucksslimey@gmail.com>
2024-11-25 13:05:04 +0300

    events done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handlechange () {
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input
+             type="password" 
+            name="INPUT"
+            placeholder="Enter password"
+            onChange={handlechange} 
+            />
+        </div>
     )
 }
 
